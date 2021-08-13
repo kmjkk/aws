b62eb443a8ae35397205e89e61563b80a44b8acb
kmjkk <alwjd1415@naver.com>
2021-08-13 16:25:45 +0900

    예제 실행

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaverRequestDto.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaverRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaverRequestDto(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
